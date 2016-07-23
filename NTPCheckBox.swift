25eeff23c8d00a0d538def78d277e7f440a90643
The Phuc <thephuc1601@gmail.com>
2016-07-23 13:55:37 +0700

    add event tap to label

--- NTPCheckBox.swift
+++ NTPCheckBox.swift
@@ -9,11 +9,11 @@ class NTPCheckBox: UIView
     private var SCREEN_WIDTH: CGFloat = CGFloat()
     private var SCREEN_HEIGHT: CGFloat = CGFloat()
     // define image for checkBox Button
-    private var checkedImage = UIImage(named: "Checked")! as UIImage
-    private var unCheckedImage = UIImage(named: "unChecked")! as UIImage
+    private var checkedImage = UIImage(named: "checked_checkbox")! as UIImage
+    private var unCheckedImage = UIImage(named: "unchecked_checkbox")! as UIImage
     // define element for checkBox Button
-    private let checkBox: UIButton = UIButton()
-    private let titleDisplay: UILabel = UILabel()
+    let checkBox: UIButton = UIButton()
+    let titleDisplay: UILabel = UILabel()
     // variable for save data when check or uncheck
     private static var data:[String:[String]] = [String:[String]]()
     private var splitStr = "####"
@@ -30,7 +30,7 @@ class NTPCheckBox: UIView
     }
     
     /* Init checkBox Button */
-    init(frame: CGRect, name: String, text: String, value: String, checked: Bool = false, titleColor: UIColor = UIColor.blackColor())
+    init(frame: CGRect, name: String, text: String, value: String, checked: Bool = false, titleColor: UIColor = UIColor(hue: 0.0389, saturation: 0, brightness: 0.36, alpha: 1.0))
     {
         super.init(frame: frame)
         // get screen size
@@ -53,19 +53,25 @@ class NTPCheckBox: UIView
         checkBox.titleLabel?.text = name + splitStr + value
         checkBox.addTarget(self, action: "tappedButton:", forControlEvents: UIControlEvents.TouchUpInside)
         // config text display
-        let heightLine = heightLabel(text, font: UIFont(name: "Arial", size: frame.height - 5)!, width: frame.width - frame.height - 5)
+        let heightLine = heightLabel(text, font: UIFont(name: "Arial", size: frame.height - 7)!, width: frame.width - frame.height - 5)
         titleDisplay.frame = CGRectMake(frame.height + 5, 0, frame.width - frame.height - 5, heightLine)
         titleDisplay.text = text
-        titleDisplay.font = UIFont(name: "Arial", size: frame.height - 5)
+        titleDisplay.font = UIFont(name: "Arial", size: frame.height - 7)
         titleDisplay.textColor = titleColor
         titleDisplay.lineBreakMode = NSLineBreakMode.ByWordWrapping
         titleDisplay.numberOfLines = 0
+        // event when tap to label
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "tappedLabel:")
+        tapGesture.numberOfTapsRequired = 1
+        titleDisplay.userInteractionEnabled =  true
+        titleDisplay.addGestureRecognizer(tapGesture)
         onChange(isChecked)
         // add element to UIView
         self.addSubview(checkBox)
         self.addSubview(titleDisplay)
     }
     
+    /*  caculate height of title display */
     private func heightLabel(text:String, font:UIFont, width:CGFloat) -> CGFloat{
         let label:UILabel = UILabel(frame: CGRectMake(0, 0, width, CGFloat.max))
         label.numberOfLines = 0
@@ -74,6 +80,41 @@ class NTPCheckBox: UIView
         label.text = text
         label.sizeToFit()
         return label.frame.height
+    }
+    
+    /* event when tap to checkbox label */
+    func tappedLabel(sender: UILabel)
+    {
+        let subperView = super.subviews
+        var name: String = ""
+        var value: String = ""
+        for sub in subperView {
+            if Mirror(reflecting: sub).subjectType == UIButton.self {
+                let button = sub as! UIButton
+                let titleLabel = button.titleLabel?.text
+                let titleLabelArray = titleLabel?.componentsSeparatedByString(splitStr)
+                name = titleLabelArray![0]
+                value = titleLabelArray![1]
+            }
+        }
+        
+        if isChecked {
+            isChecked = false
+            let index = NTPCheckBox.data[name]?.indexOf(value)
+            if index != nil {
+                NTPCheckBox.data[name]?.removeAtIndex(index!)
+            }
+        } else {
+            isChecked = true
+            if NTPCheckBox.data[name]?.count != nil {
+                var keepArr = NTPCheckBox.data[name]
+                keepArr?.append(value)
+                NTPCheckBox.data[name] = keepArr
+            } else {
+                NTPCheckBox.data[name] = [value]
+            }
+        }
+        onChange(isChecked)
     }
     
     /* event when tap to checkBox Button */
@@ -111,11 +152,11 @@ class NTPCheckBox: UIView
         return [] // return empty array
     }
     
-    deinit{
-        NTPCheckBox.data.removeAll()
-    }
-    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit{
+        NTPCheckBox.data.removeAll()
     }
 }
--- NTPRadio.swift
+++ NTPRadio.swift
@@ -12,15 +12,15 @@ class NTPRadio: UIView
     private var checkedImage = UIImage(named: "Selected")! as UIImage
     private var unCheckedImage = UIImage(named: "unSelected")! as UIImage
     // define element for radio Button
-    private let radio: UIButton = UIButton()
-    private let titleDisplay: UILabel = UILabel()
+    let radio: UIButton = UIButton()
+    let titleDisplay: UILabel = UILabel()
     // variable for save data for selected
     private static var data:[String:String] = [String:String]()
     private static var isSelected:Bool = false
     private var splitStr = "####"
     
     /* Init radio Button */
-    init(frame: CGRect, name: String, text: String, value: String, selected: Bool = false, titleColor: UIColor = UIColor.blackColor())
+    init(frame: CGRect, name: String, text: String, value: String, selected: Bool = false, titleColor: UIColor = UIColor(hue: 0.0389, saturation: 0, brightness: 0.36, alpha: 1.0))
     {
         super.init(frame: frame)
         // get screen size
@@ -43,18 +43,24 @@ class NTPRadio: UIView
         radio.titleLabel?.text = name + splitStr + value
         radio.addTarget(self, action: "tappedButton:", forControlEvents: UIControlEvents.TouchUpInside)
         // config text display
-        let heightLine = heightLabel(text, font: UIFont(name: "Arial", size: frame.height - 5)!, width: frame.width - frame.height - 5)
-        titleDisplay.frame = CGRectMake(frame.height + 5, 0, frame.width - frame.height - 5, heightLine)
+        let heightLine = heightLabel(text, font: UIFont(name: "Arial", size: frame.height - 7)!, width: frame.width - frame.height - 5)
+        titleDisplay.frame = CGRectMake(frame.height + 7, 0, frame.width - frame.height - 5, heightLine)
         titleDisplay.text = text
-        titleDisplay.font = UIFont(name: "Arial", size: frame.height - 5)
+        titleDisplay.font = UIFont(name: "Arial", size: frame.height - 7)
         titleDisplay.textColor = titleColor
         titleDisplay.lineBreakMode = NSLineBreakMode.ByWordWrapping
         titleDisplay.numberOfLines = 0
+        // event when tap to label
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "tappedLabel:")
+        tapGesture.numberOfTapsRequired = 1
+        titleDisplay.userInteractionEnabled =  true
+        titleDisplay.addGestureRecognizer(tapGesture)
         // add element to UIView
         self.addSubview(radio)
         self.addSubview(titleDisplay)
     }
     
+    /*  caculate height of title display */
     private func heightLabel(text:String, font:UIFont, width:CGFloat) -> CGFloat{
         let label:UILabel = UILabel(frame: CGRectMake(0, 0, width, CGFloat.max))
         label.numberOfLines = 0
@@ -63,6 +69,43 @@ class NTPRadio: UIView
         label.text = text
         label.sizeToFit()
         return label.frame.height
+    }
+    
+    /* event when tap to radio label */
+    func tappedLabel(sender: UILabel)
+    {
+        let subperView = super.subviews
+        var nameRadio: String = ""
+        var currentSelected: String = ""
+        for sub in subperView {
+            if Mirror(reflecting: sub).subjectType == UIButton.self {
+                let button = sub as! UIButton
+                let titleLabel = button.titleLabel?.text
+                let titleLabelArray = titleLabel?.componentsSeparatedByString(splitStr)
+                nameRadio = titleLabelArray![0]
+                currentSelected = titleLabelArray![1]
+            }
+        }
+        
+        for sub in (self.superview?.subviews)! {
+            if Mirror(reflecting: sub).subjectType == NTPRadio.self {
+                for subRadio in sub.subviews {
+                    if Mirror(reflecting: subRadio).subjectType == UIButton.self {
+                        let button = subRadio as! UIButton
+                        let titleLabel = button.titleLabel?.text
+                        let titleLabelArray = titleLabel?.componentsSeparatedByString(splitStr)
+                        if nameRadio == titleLabelArray![0] as String {
+                            if currentSelected == titleLabelArray![1] as String {
+                                button.setBackgroundImage(checkedImage, forState: .Normal)
+                                NTPRadio.data[nameRadio] = currentSelected
+                            } else {
+                                button.setBackgroundImage(unCheckedImage, forState: .Normal)
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
     
     /* event when tap to radio button */
@@ -102,6 +145,7 @@ class NTPRadio: UIView
         return ""
     }
     
+    /* destruct radio */
     deinit {
         NTPRadio.data.removeAll()
     }
